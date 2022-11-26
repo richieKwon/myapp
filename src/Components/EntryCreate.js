eee92a9179187fe3082672c90ee2e845e4bbc9b5
richie <richie.kwon1@gmail.com>
2022-11-26 21:18:50 +0000

    create component is done

--- src/Components/EntryCreate.js
+++ src/Components/EntryCreate.js
@@ -1,34 +1,35 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
-import useFetch from "./UseFetch";
 
 export default function EntryCreate(props) {
     const [status, setStatus] = useState({
-        id: 0,
+        id: "",
         name: "",
         title: "",
         content: "",
-        created: null
+        created: ""
     });
     const navigate = useNavigate()
 
     function handleSubmit(event) {
         event.preventDefault();
         let entryDto = {
-            id: 0,
+            // id: 0,
             name: status.name,
             title: status.title,
             content: status.content,
-            created: Date.now()
+            // created: Date.now()
         };
+
+        console.log(entryDto);
 
         fetch("https://localhost:5001/api/entries", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Headers': '*',
-                "Access-Control-Allow-Methods": 'OPTIONS,POST,GET'
+                'Access-Control-Allow-origin': '*',
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             },
             body: JSON.stringify(entryDto)
         }).then(res => {
@@ -41,16 +42,25 @@ export default function EntryCreate(props) {
         //     .then((result) => { navigate('/') })
     }
 
-    function handleChangeName(event) {
-        setStatus({ name: event.target.value })
+    const handleChangeName = (event) => {
+        setStatus({
+            ...status,
+            name: event.target.value
+        })
     }
 
     function handleChangeTitle(event) {
-        setStatus({ title: event.target.value })
+        setStatus({
+            ...status,
+            title: event.target.value
+        })
     }
 
     function handleChangeContent(event) {
-        setStatus({ content: event.target.value })
+        setStatus({
+            ...status,
+            content: event.target.value
+        })
     }
 
     return (
@@ -62,19 +72,18 @@ export default function EntryCreate(props) {
                         <div className="form-group">
                             <label style={{ display: "flex", flexDirection: "row", paddingLeft: 12 }}>Name</label>
                             <input type="text" className="form-control" placeholder="Add Name" value={status.name}
-                                onChange={handleChangeName} style={{ margin: 10 }} />
+                                onChange={handleChangeName} style={{ margin: 10 }} name="name"></input>
                         </div>
                         <div className="form-group">
                             <label style={{ display: "flex", flexDirection: "row", paddingLeft: 12 }}>Title</label>
                             <input type="text" className="form-control" placeholder="Add Title" value={status.title}
-                                onChange={handleChangeTitle} style={{ margin: 10 }} />
+                                onChange={handleChangeTitle} style={{ margin: 10 }} name="title"></input>
                         </div>
                         <div className="form-group">
                             <label style={{ display: "flex", flexDirection: "row", paddingLeft: 12 }}>Content</label>
                             <textarea className="form-control" placeholder="Put down the story" value={status.content}
                                 onChange={handleChangeContent} style={{ margin: 10 }} rows="10">
                             </textarea>
-
                         </div>
                         <div className="form-group">
                             <button type="submit" className="btn btn-primary">Submit</button>
